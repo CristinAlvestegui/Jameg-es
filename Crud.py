b793826be2b033b9202bbf62fe5b35e0ae8203fa
CristinAlvestegui <quitina.23@gmail.com>
2022-07-05 15:16:13 -0300

    vem menuu

--- Crud.py
+++ Crud.py
@@ -7,11 +7,11 @@ con = db_connect.cursor()
 def inseriJame(dado):
      #aqui deve ser qualquer foto
     try:
-        abrirPasta()
+        #abrirPasta(docu)
         sql = "insert into Jamegoes(codigo, dado) values('', {})".format(dado)
         con.execute(sql)
         db_connect.commit()
-        docu.save() #'foto que foi selecionada deve ser salval com o jamego'
+        #docu.save() #'foto que foi selecionada deve ser salval com o jamego'
         print("{} Inserido".format(con.rowcount))
     except Exception as erro:
             print(erro)
@@ -25,18 +25,18 @@ def consuJame():
     except Exception as erro:
         print(erro)
 
-def atualiJame(cod, campo, novoDado):
+def atualiJame(dado, campo, novoDado):
     try:
-        sql = "update Jamegoes set {} = '{}' where codigo = '{}'".format(cod, campo, novoDado)
+        sql = "update Jamegoes set {} = '{}' where codigo = '{}'".format(dado, campo, novoDado)
         con.execute(sql)
         db_connect.commit()
         print('{} Atualizado!'.format(con.rowcount))
     except Exception as erro:
         print(erro)
 
-def excluirJame(cod):
+def excluirJame(dado):
     try:
-        sql = "delete from Jamegoes where codigo = '{}'".format(cod)
+        sql = "delete from Jamegoes where codigo = '{}'".format(dado)
         con.execute(sql)
         db_connect.commit()
         print('{} Excluido!'.format(con.rowcount))
@@ -45,10 +45,16 @@ def excluirJame(cod):
 
 def abrirPasta():
     try:
-        docu = open(r'C:\Users\cristina.asubieta\Documents\Fotos\foto7.jpg', 'r')
-        arti = docu.read()
+        file = open('C:\Users\cristina.asubieta\Documents', 'r')
+        arti = file.read()
         print(arti)
     except Exception as erro:
         print(erro)
     else:
-        docu.close()
+        file.close()
+
+def jamegarFile():
+    #aqui devemos marcar esse arquivo com o nosso jamego
+    if abrirPasta().open:
+        docu = open('', 'r')  # deve ser qualquer documento que usuário selecionar
+
--- Menu.py
+++ Menu.py
@@ -3,18 +3,41 @@ import Crud
 
 this.opcao = -1
 
-def menu():
+def menu1():
     print('\nEscolha umas das opções abaixo: \n\n' +
-        '1. Cadastrar Jamegão\n'                   +
-        '2. Consultar Jamegão\n'                   +
-        '3. Atualizar Jamegão\n'                   +
-        '4. Desativar Jamegão\n'                   +
+        '1. Abrir Fotos\n'                         +
+        '2. Abrir Documentos\n'                    +
+        '3. Abrir Aplicativos\n'                   +
         '0. Sair\n')
     this.opcao = int(input())
 
-def operacoes():
+def menu2():
+    print('\nEscolha umas das alternativas abaixo: \n\n' +
+        '1. Cadastrar Jamegão\n'                         +
+        '2. Consultar Jamegão\n'                         +
+        '3. Editar Jamegão\n'                            +
+        '4. Desativar Jamegão\n'                         +
+        '0. Sair\n')
+    this.opcao = int(input())
+
+def executarop1():
+    while(this.opcao !=0):
+        menu1()
+        if this.opcao == 1:
+            print('Elemento ainda em desenvolvimento...agradecemos a sua compreensão')
+        elif this.opcao == 2:
+            print('Por favor selecione um dos arquivos de teste disponives na pasta a seguir:')
+            Crud.abrirPasta()
+
+            docu = input()
+
+            executarop2()
+        elif this.opcao == 3:
+            print('Componente ainda em desenvolvimento...agradecemos a sua compreensão')
+
+def executarop2():
     while(this.opcao != 0):
-        menu()
+        menu2()
         if this.opcao == 1:
             print('Imforme o nome do novo Jamego: ')
             dado = input()
